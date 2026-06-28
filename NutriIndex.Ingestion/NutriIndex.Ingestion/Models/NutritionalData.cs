31cdc4d2f4d178fb092868397422324348aed4f8
Apostolos Draganis <draganisdev@gmail.com>
2026-06-28 17:26:50 +0200

    remove fields from NutritionalData. Will add later

--- NutriIndex.Ingestion/NutriIndex.Ingestion/Models/NutritionalData.cs
+++ NutriIndex.Ingestion/NutriIndex.Ingestion/Models/NutritionalData.cs
@@ -4,9 +4,5 @@ public record NutritionalData(
     string ProductName,
     string? Brand,
     double EnergyKcalPer100g,
-    double ProteinGramsPer100g,
-    double? FatGramsPer100g,
-    double? CarbohydratesGramsPer100g,
-    double? SodiumMilligramsPer100g,
-    List<string> Categories
+    double ProteinGramsPer100g
 );
